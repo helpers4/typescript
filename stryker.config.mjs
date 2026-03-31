ddfed5555f169e28c7efc8ef17f316f3f8b76bf4
baxyz <baxyz@etik.com>
2026-03-31 22:21:27 +0000

    chore(config): 🔧 add stryker configuration for mutation testing

---
+++ stryker.config.mjs
@@ -0,0 +1,28 @@
+/**
+ * This file is part of helpers4.
+ * Copyright (C) 2025 baxyz
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+export default {
+  testRunner: 'vitest',
+  tsconfigFile: 'tsconfig.json',
+  plugins: [
+    '@stryker-mutator/vitest-runner',
+  ],
+  mutate: [
+    'helpers/**/!(*.test|*.spec|*.bench|*.example|index).ts',
+  ],
+  reporters: ['clear-text', 'html', 'progress'],
+  htmlReporter: {
+    fileName: 'reports/mutation/index.html',
+  },
+  thresholds: {
+    high: 90,
+    low: 70,
+    break: 60,
+  },
+  tempDirName: '.stryker-tmp',
+  cleanTempDir: 'always',
+};
