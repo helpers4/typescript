b76a61e2d6c51f3235b81ea0c0defd8c13ac5050
baxyz <baxyz@etik.com>
2026-05-02 21:26:59 +0000

    feat(array): ✨ add intersects helper and related tests/examples

--- helpers/array/intersects.test.ts
+++ helpers/array/intersects.test.ts
@@ -5,18 +5,18 @@
  */
 
 import { describe, expect, it } from 'vitest';
-import { oneInCommon } from './oneInCommon';
+import { intersects } from './intersects';
 
-describe('oneInCommon', () => {
+describe('intersects', () => {
     it('should return true if there is at least one item in common', () => {
-        expect(oneInCommon([1, 2, 3], [3, 4, 5])).toBe(true);
+        expect(intersects([1, 2, 3], [3, 4, 5])).toBe(true);
     });
 
     it('should return false if there are no items in common', () => {
-        expect(oneInCommon([1, 2, 3], [4, 5, 6])).toBe(false);
+        expect(intersects([1, 2, 3], [4, 5, 6])).toBe(false);
     });
 
     it('should return false if one of the arrays is empty', () => {
-        expect(oneInCommon([1, 2, 3], [])).toBe(false);
+        expect(intersects([1, 2, 3], [])).toBe(false);
     });
 });
